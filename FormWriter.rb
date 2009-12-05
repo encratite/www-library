f72d5115fd62a8ba0faa28aff034c0b303a820de
void <git.email.address@googlemail.com>
2009-12-05 18:08:12 +0100

    Further form writer fixes

--- FormWriter.rb
+++ FormWriter.rb
@@ -4,9 +4,16 @@ class FormWriter
 		@output.concat "<form action=\"#{action}\" method=\"post\">\n"
 	end
 	
-	def label(label, type = 'text', name = label.downcase, id = name)
-		passwordString = 'password'
-		type = passwordString if label.downcase.include? passwordString
+	def label(arguments = {})
+		label = arguments[:label] || 'Description'
+		type = arguments[:type]
+		name = arguments[:name] || label.downcase
+		id = arguments[:id] || name
+		
+		if type == nil
+			passwordString = 'password'
+			type = label.downcase.include? passwordString ? passwordString : 'text'
+		end
 		@output.concat "<p>\n<label for=\"#{id}\">#{label}:</label><br />\n<input type=\"#{type}\" id=\"#{id}\" name=\"#{name}\" />\n</p>\n"
 	end
 
