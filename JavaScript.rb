f8763fe4dc1ad9578425da69a6f692e2e4149cab
void <git.email.address@googlemail.com>
2009-12-20 06:17:01 +0100

    Changed the name of the JavaScript function

--- JavaScript.rb
+++ JavaScript.rb
@@ -1,3 +1,3 @@
 def writeJavaScript(input)
-	output = "<script type=\"text/javascript\">\n//<![CDATA[\n#{input}\n//]]>\n</script>"
+	output = "<script type=\"text/javascript\">\n//<![CDATA[\n#{input}\n//]]>\n</script>\n"
 end
