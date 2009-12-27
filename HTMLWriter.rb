41ea242e685d2519a9af869eea9482f7fe189b1a
void <git.email.address@googlemail.com>
2009-12-27 07:35:33 +0100

    This is some messed up stuff

--- HTMLWriter.rb
+++ HTMLWriter.rb
@@ -1,3 +1,13 @@
+class SelectOption
+	attr_reader :description, :value
+	attr_accessor :selected
+	def initialize(description, value, selected = false)
+		@description = description
+		@value = value
+		@selected = selected
+	end
+end
+
 class HTMLWriter
 	def initialize(output)
 		@output = output
@@ -10,17 +20,22 @@ class HTMLWriter
 		return nil
 	end
 	
-	def tag(tag, arguments, block, useNewline = true)
+	def tag(tag, arguments, function = nil, useNewline = true)
 		newline = "\n"
 		writeNewline = lambda { write newline if useNewline }
+		
 		argumentString = ''
 		arguments.each { |key, value| argumentString += " #{key.to_s}=\"#{value}\"" }
-		write "<#{tag}#{argumentString}>"
-		writeNewline.call
-		data = block.call
-		write data if data.class == String
-		writeNewline.call if @lastCharacter != newline
-		write "</#{tag}>"
+		if function == nil
+			write "<#{tag}#{argumentString} />"
+		else
+			write "<#{tag}#{argumentString}>"
+			writeNewline.call
+			data = block.call
+			write data if data.class == String
+			writeNewline.call if @lastCharacter != newline
+			write "</#{tag}>"
+		end
 		writeNewline.call
 		return nil
 	end
@@ -33,11 +48,91 @@ class HTMLWriter
 		end
 	end
 	
+	def form(action, arguments = {}, &block)
+		arguments[:method] = 'post'
+		arguments[:action] = action
+		tag('form', arguments, block)
+	end
+	
+	def withLabel(label, &block)
+		ul class: formLabel do
+			li { label }
+			li { block.call }
+		end
+	end
+	
+	def field(type, label, name, value, arguments)
+		arguments[:type] = type
+		arguments[:name] = name
+		arguments[:value] = value
+		
+		withLabel label { tag('input', arguments) }
+	end
+	
+	def text(label, name, value = nil, arguments = {})
+		field('text', label, name, value, arguments)
+	end
+	
+	def password(label, name, value = nil, arguments = {})
+		field('password', label, name, value, arguments)
+	end
+	
+	def hidden(name, value = nil, arguments = {})
+		arguments[:type] = 'hidden'
+		arguments[:name] = name
+		arguments[:value] = value
+		
+		tag('input', arguments)
+	end
+	
+	def radio(label, name, value, checked = false, arguments = {})
+		arguments[:type] = 'radio'
+		arguments[:name] = name
+		arguments[:value] = value
+		arguments[:checked] = 'checked' if checked
+		
+		tag('input', arguments, nil, false)
+		write " #{label}\n"
+	end
+	
+	def select(name, options, arguments = {})
+		function = lambda do
+			gotASelection = false
+			options.each do |option|
+				currentArguments = {value: option.value}
+				if option.selected
+					raise 'You cannot specify more than one selected element in a <select> tag.' if gotASelection
+					gotASelection = true
+					current_arguments[:selected] = 'selected'
+				end
+				option currentArguments { option.description }
+			end
+		end
+		arguments[:name] = name
+		tag('select', arguments, function)
+	end
+	
+	def textArea(name, value, arguments = {})
+		function = lambda { value }
+		arguments[:name] = name
+		tag('textarea', arguments, function)
+	end
+	
+	def submit(description = 'Submit')
+		arguments[:type] = 'submit'
+		arguments[:value] = description
+		
+		p do
+			tag('input', arguments)
+		end
+	end
+	
 	self.createMethods [
 		'a',
 		'b',
 		'div',
 		'li',
+		'option',
 		'p',
 		'table',
 		'td',
