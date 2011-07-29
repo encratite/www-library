41462380079fb49b3b00cc85546e6dcb224d01c6
void <git.email.address@googlemail.com>
2011-07-29 19:36:19 +0200

    Fixed up the form fields

--- FormFields.rb
+++ FormFields.rb
@@ -1,17 +1,21 @@
+require 'nil/symbol'
+
 module WWWLib
   class FormFields
     attr_reader :error
 
     def initialize(request)
       @error = false
-      self.constants.each do |symbol|
-        name = self.const_get(symbol)
+      self.class.constants.each do |symbol|
+        name = self.class.const_get(symbol)
         value = request.getPost(symbol)
         if value == nil
           @error = true
           return true
         end
-        
+        symbolString = symbol.to_s
+        translatedSymbol = symbolString[0].downcase + symbolString[1..-1]
+        setPublicMember(translatedSymbol, value)
       end
     end
   end
